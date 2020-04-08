bce94d4283e31755263c6acdfc8fcd0247996a74
jle <2377388994@qq.com>
2020-04-08 15:11:10 +0800

    创建数据库

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,10 @@ from .forms import LoginForm
 from flask import render_template
 
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+
     return render_template('auth/login.html', form=form)
