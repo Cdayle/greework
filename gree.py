af2b353844833aecb6451bb33ba36ad3060dcfb9
jle <2377388994@qq.com>
2020-04-08 14:17:46 +0800

    创建数据库

--- gree.py
+++ gree.py
@@ -1,12 +1,14 @@
-from app import create_app,db
+from app import create_app, db
 import os
-
+from flask_migrate import Migrate
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-# migrate = Migrate(app,db)
+migrate = Migrate(app, db)
+
 
 # cmd 启动示例
-# @app.shell_content_processor
-# def make_shell_context():
-# 	return dic(db=db, User=User,Role=Role)
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
