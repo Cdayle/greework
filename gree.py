d62482a5563070a1b2ddeb6508aa9e5cdcc3dcc5
jle <2377388994@qq.com>
2020-05-02 11:08:08 +0800

    添加用户权限

--- gree.py
+++ gree.py
@@ -1,7 +1,8 @@
 from app import create_app, db
 import os
 from flask_migrate import Migrate
-from app.models import User
+from app.models import User,Role
+import click
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -12,3 +13,15 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User)
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # # migrate database to latest revision
+    # upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follows()
